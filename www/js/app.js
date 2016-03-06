7fcd23f02d11dab6c5b342cd11e89c3a226d28ce
Tyler <fletchertyler914@yahoo.com>
2016-03-05 23:44:36 -0500

    Updated Paths

--- www/js/app.js
+++ www/js/app.js
@@ -17,39 +17,47 @@ angular.module('app', ['ionic', 'app.controllers','firebase'])
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+      StatusBar.hide();
     }
+      
   });
 })
 
+.factory("Auth", function($firebaseAuth) {
+  var usersRef = new Firebase("https//90daysapp.firebaseio.com/users");
+  return $firebaseAuth(usersRef);
+})
 
 .config(function($stateProvider, $urlRouterProvider) {
+    
+    var ref = new Firebase("https://90daysapp.firebaseio.com");
+    var authData = ref.getAuth();
+    
+    
+//   if (authData) {
 
-  $stateProvider
+      //console.log("User " + authData.uid + " is logged in with " + authData.provider);
+    
+        $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'www/templates/menu.html',
-    controller: 'AppCtrl'
-  })
+            .state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: 'www/templates/menu.html',
+            controller: 'AppCtrl', 
+          })
 
-    .state('app.addItem', {
-    url: '/addItem',
-    abstract: true,
-    templateUrl: 'www/templates/addItem.html',
-  })
+          .state('app.home', {
+              url: '/home',
+              views: {
+                'menuContent': {
+                  templateUrl: 'www/templates/home.html',
+                  controller: 'homeCtrl'
+                }
+              },
+            });
+        
+          // if none of the above states are matched, use this as the fallback
+          $urlRouterProvider.otherwise('/app/home');
 
-  .state('app.home', {
-      url: '/home',
-      views: {
-        'menuContent': {
-          templateUrl: 'www/templates/home.html',
-          controller: 'homeCtrl'
-        }
-      },
     });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/home');
-});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,24 +1,98 @@
 angular.module('app.controllers', ['ngAnimate'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $rootScope) {
+    
+    // Create a callback which logs the current auth state
+    function authDataCallback(authData) {
+      if (authData) {
+        //console.log("User " + authData.uid + " is logged in with " + authData.provider);
+        // Assign a scope variable to the authinticated user
+        $rootScope.authDataUid = authData.uid;
+        $rootScope.SignedIn = true;
 
+      } else {
+        //console.log("User is logged out");
+        $rootScope.SignedIn = false;
+      }
+    }
+
+    // Register the callback to be fired every time auth state changes
+    var ref = new Firebase("https://90daysapp.firebaseio.com");
+    ref.onAuth(authDataCallback);
+
+    
   // Form data for the login modal
   $scope.itemData = {};
+  $scope.loginData = {};
+  //$scope.signupData = {};
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('www/templates/login.html', {
     scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+  }).then(function(loginModal) {
+    $scope.loginModal = loginModal;
   });
   // Triggered in the login modal to close it
   $scope.closeLogin = function() {
-    $scope.modal.hide();
+    $scope.loginModal.hide();
   };
   // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
+  $scope.openLogin = function() {
+    $scope.loginModal.show();
   };
+    
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    var ref = new Firebase("https://90daysapp.firebaseio.com");
+    ref.authWithPassword({
+      email    : $scope.loginData.username,
+      password : $scope.loginData.password
+    }, function(error, authData) {
+      if (authData) {
+        console.log("Successfully logged in with uid:", authData.uid);
+          $scope.closeLogin();
+      } else {
+        console.log("Error Logging In: ", error);
+        alert("Error: ", error);
+      }
+    });
+  };
+    
+// Create the signup modal that we will use later
+  $ionicModal.fromTemplateUrl('www/templates/signup.html', {
+    scope: $scope
+  }).then(function(signupModal) {
+    $scope.signupModal = signupModal;
+  });
+  // Triggered in the signup modal to close it
+  $scope.closeSignup = function() {
+    $scope.signupModal.hide();
+  };
+  // Open the signup modal
+  $scope.openSignup = function() {
+    $scope.signupModal.show();
+  };
+    
+  // Perform the signup action when the user submits the signup form
+  $scope.doSignup = function() {
+    var ref = new Firebase("https://90daysapp.firebaseio.com");
+    ref.createUser({
+      email    : $scope.signupData.username,
+      password : $scope.signupData.password
+    }, function(error, userData) {
+      if (userData) {
+        //console.log("Successfully created user account with uid:", userData.uid);
+          $scope.closeSignup();
+      } else {
+        //console.log("Error Signing Up: ", error);
+        alert("Error: ", error);
+      }
+    });
+  };
+    
+    $scope.signout = function(){
+        ref.unauth();
+    };
 
   // Create the addItem modal that we will use later
   $ionicModal.fromTemplateUrl('www/templates/addItem.html', {
@@ -35,36 +109,55 @@ angular.module('app.controllers', ['ngAnimate'])
     $scope.addItemModal.show();
   };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.itemData);
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+}) // End App Ctrl
+
+// Possibly needed?
 .factory("ItemsArray", function($firebaseArray) {
   var itemsRef = new Firebase("https://90DaysApp.firebaseio.com/items");
   return $firebaseArray(itemsRef);
 })
 
 .controller('homeCtrl', function($scope, ItemsArray, $firebaseObject, $firebaseArray) {
+    if(window.navigator.standalone == true) {
+  // My app is installed and therefore fullscreen
+        //alert("test");
+}
+    
   // Get a reference to our Items
   var ref = new Firebase("https://90DaysApp.firebaseio.com/items");
   // Retrieve new posts as they are added to our database
   ref.on("value", function(getObject, prevChildKey) {
     var getData = getObject.val();
-    $scope.FoodUrl = getData.Food;
-    $scope.SmoothieUrl = getData.Smoothie;
+     
+    // Set Data value's to a variable for inAppBrowser to Understand  
+    var foodUrl = getData.Food;
+    var smoothieUrl = getData.Smoothie;
+    var workoutUrl = getData.Workout;
+      
+      $scope.openFoodUrl = function()
+        {
+         // Open in app browser
+         window.open(foodUrl,'_blank');
+        };
+
+        $scope.openSmoothieUrl = function()
+        {
+         // Open in app browser
+         window.open(smoothieUrl,'_blank');
+        };
+      
+      $scope.openWorkoutUrl = function()
+        {
+         // Open in app browser
+         window.open(workoutUrl,'_blank');
+        };
   });
 
   $scope.items = [
-    { title: 'Smoothie of The Day', id: 1 },
-    { title: 'Food of The Day', id: 2 },
-    { title: 'Workout of The Day', id: 3 },
-    { title: 'Join Me!', id: 4 },
+    { title: 'Smoothie of The Day: ', id: 1, link: "Smoothie", image: "https://dl.dropboxusercontent.com/u/20609233/90days/www/img/blender.png"},
+    { title: 'Food of The Day: ', id: 2, link: "Food", image: "https://dl.dropboxusercontent.com/u/20609233/90days/www/img/food.png" },
+    { title: 'Workout of The Day: ', id: 3, link: "Workout", image: "https://dl.dropboxusercontent.com/u/20609233/90days/www/img/barbell.png"},
+//    { title: 'Join Me!', id: 4, link: "JoinMe" },
   ];
 })
 
@@ -78,25 +171,10 @@ angular.module('app.controllers', ['ngAnimate'])
       if ($scope.itemData.food){
         itemsRef.update({Food: $scope.itemData.food})
       }
+      if ($scope.itemData.workout){
+        itemsRef.update({Workout: $scope.itemData.workout})
+      }
       $scope.closeAddItem();
     };
   })
 
-  .controller("inAppCtrl", function ($scope) {
-    var foodUrl = $scope.foodUrl;
-    var url = "wwww.google.com";
-
-
-    $scope.openInAppBrowser = function()
-    {
-     // Open in app browser
-     window.open(foodUrl,'_blank');
-    };
-
-    $scope.openCordovaWebView = function()
-    {
-     // Open cordova webview if the url is in the whitelist otherwise opens in app browser
-     window.open(foodUrl,'_self');
-    };
-
-  });
